71044cbf0ce08560ecdf5717eb9309cac17717e3
Thomas Röggla <t.roggla@cwi.nl>
2023-07-27 15:34:33 +0000

    updated build script to copy build libs to current directory

--- build_cwipc_android.sh
+++ build_cwipc_android.sh
@@ -14,3 +14,25 @@ conan create conanfiles/libjpeg-turbo --profile android -s arch=$ARCH --build=mi
 
 echo "Building cwipc..."
 conan create conanfiles/cwipc --profile android -s arch=$ARCH --build=missing
+
+echo "Copying compiled artifacts..."
+conan list 'cwipc#:*' |sed 's/ //g' > package_info.txt
+
+package_name=`cat package_info.txt |head -n3 |tail -n1`
+revision_id=`cat package_info.txt |head -n5 |tail -n1 |sed 's/(.*)//'`
+package_id=`cat package_info.txt |head -n7 |tail -n1`
+
+package_folder=`conan cache path ${package_name}#${revision_id}:${package_id}`
+
+if [ -d "dist/${ARCH}" ]; then
+    echo "Deleting outdated dist directory..."
+    rm -r dist/${ARCH}
+fi
+
+mkdir -p dist/${ARCH}/lib dist/${ARCH}/include
+cp ${package_folder}/lib/*.so dist/${ARCH}/lib
+cp -r ${package_folder}/include/* dist/${ARCH}/include
+
+rm package_info.txt
+
+echo "DONE!"
